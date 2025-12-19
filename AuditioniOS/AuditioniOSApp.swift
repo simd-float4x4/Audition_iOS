9277b9e7d02a3123bf605d9c693d353b7552e6e6
simd-float4x4 <asobu.developers+mathew@gmail.com>
2025-12-19 11:22:32 +0900

    initial commit

---
+++ AuditioniOS/AuditioniOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuditioniOSApp.swift
+//  AuditioniOS
+//
+//  Created by Shumpei Horiuchi on 2025/12/19.
+//
+
+import SwiftUI
+
+@main
+struct AuditioniOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
---
+++ AuditioniOS/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  AuditioniOS
+//
+//  Created by Shumpei Horiuchi on 2025/12/19.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
