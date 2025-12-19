d3745fe78a00d9f98ea80ba0c94b08afcf9c9ae1
simd-float4x4 <asobu.developers+mathew@gmail.com>
2025-12-19 11:46:07 +0900

    fix: README.mdにソフトウェア要件を追加

--- AuditioniOS/LoginView.swift
+++ AuditioniOS/LoginView.swift
@@ -9,12 +9,6 @@ import SwiftUI
 
 struct LoginView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        
     }
 }
